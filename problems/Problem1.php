965b3f6222dd368e8afeb7c2f82df843f1f2178e
Bjørn Madsen <bm@aeons.dk>
2015-02-24 11:16:40 +0000

    Problem 1

--- problems/Problem1.php
+++ problems/Problem1.php
@@ -4,6 +4,12 @@ namespace Euler\Problems;
 class Problem1 implements IProblem
 {
     public function solve() {
-        return 1337;
+        $sum = 0;
+        for ($i = 1; $i < 1000; $i++) {
+            if ($i % 3 == 0 || $i % 5 == 0) {
+                $sum += $i;
+            }
+        }
+        return $sum;
     }
 }
